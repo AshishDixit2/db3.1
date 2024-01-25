f1766fbd59a388b4e4e1bd8952bb44f70583c9e3
AshishDixit2 <157109472+AshishDixit2@users.noreply.github.com>
2024-01-25 19:02:28 +0530

    Add files via upload

---
+++ Student/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Student
+{
+    public class Studentdetail
+    {
+    public string Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Address { get; set; }
+    public int StudentId { get; set; }
+}
+}
